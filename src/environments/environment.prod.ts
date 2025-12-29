099e0b004af5027debb31004001fe5305fb38ea5
S Kh <hristow.stefan@gmail.com>
2025-12-29 13:52:35 +0200

    env auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  // Production backend base URL
-  apiBase: 'https://betlite-be.onrender.com'
+  apiBase: 'https://betlite-be.onrender.com',
+  supabaseUrl: 'https://fhscqyikizzcdjonczln.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoc2NxeWlraXp6Y2Rqb25jemxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxODA5NjcsImV4cCI6MjA3NTc1Njk2N30.Veapr0lFdeSuHWA3WNwCM93sENeNxxi688d9bK34KxE'
 };
